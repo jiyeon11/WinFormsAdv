d2a8f33afbd318fdfb6456f6d2a99ba1b099fa50
jiyeon11 <leejiyeon415@gmail.com>
2024-08-22 11:18:48 +0900

    윈폼 메뉴 작성

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace WinFormsAdv
         {
             InitializeComponent();
         }
+
+        private void 끈내기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
