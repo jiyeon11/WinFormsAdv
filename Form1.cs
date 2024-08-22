203d6f1d26791acf33ebf39fb6ae5bfdd99fe1a7
jiyeon11 <leejiyeon415@gmail.com>
2024-08-22 11:58:19 +0900

    보기 > 확대/축소하기 구현

--- Form1.cs
+++ Form1.cs
@@ -26,5 +26,35 @@ namespace WinFormsAdv
         {
             System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=TLut9aAQNvs");
         }
+
+        private void 확대하기축소하기기본값복원ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStripProgressBar1.Value = 50;
+        }
+
+        private void 확대ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(toolStripProgressBar1.Maximum < toolStripProgressBar1.Value + 10)
+            {
+                toolStripProgressBar1.Value = toolStripProgressBar1.Maximum;
+            }
+            else
+            {
+                toolStripProgressBar1.Value += toolStripProgressBar1.Step;
+            }
+            
+        }
+
+        private void 축소ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (toolStripProgressBar1.Minimum > toolStripProgressBar1.Value - toolStripProgressBar1.Step)
+            {
+                toolStripProgressBar1.Value = toolStripProgressBar1.Minimum;
+            }
+            else
+            {
+                toolStripProgressBar1.Value -= toolStripProgressBar1.Step;
+            }
+        }
     }
 }
